51d01c4dd3623c0860e877c0be31f85f0b8ef806
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2023-11-25 06:56:51 +0100

    Adding examples

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
 const canonize = require('rdf-canonize');
+const stringifyStream = require('stream-to-string');
+const streamifyString = require('streamify-string');
+const rdfParser = require("rdf-parse").default;
+const rdfSerializer = require("rdf-serialize").default;
+const md5 = require('md5');
+const fs = require('fs');
 
+const input = process.argv[2];
 
-const nquads = `
-_:genid1 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://rule.org/ForwardRule> .
-_:genid1 <http://rule.org/premise> _:genid4 .
-_:genid1 <http://rule.org/vars> _:genid2 .
-_:genid2 <http://www.w3.org/1999/02/22-rdf-syntax-ns#first> _:genid3 .
-_:genid2 <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest> <http://www.w3.org/1999/02/22-rdf-syntax-ns#nil> .
-_:genid5 <http://www.w3.org/1999/02/22-rdf-syntax-ns#first> _:genid3 .
-_:genid5 <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest> _:genid6 .
-_:genid6 <http://www.w3.org/1999/02/22-rdf-syntax-ns#first> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> .
-_:genid6 <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest> _:genid7 .
-_:genid7 <http://www.w3.org/1999/02/22-rdf-syntax-ns#first> _:genid8 .
-_:genid7 <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest> <http://www.w3.org/1999/02/22-rdf-syntax-ns#nil> .
-_:genid4 <http://www.w3.org/1999/02/22-rdf-syntax-ns#first> _:genid5 .
-_:genid4 <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest> <http://www.w3.org/1999/02/22-rdf-syntax-ns#nil> .
-`;
+if (! input) {
+    console.error(`usage: index.js file`);
+    process.exit(1);
+}
 
-main();
+main(input);
 
-async function main() {
+async function main(path) {
+    const data = fs.readFileSync(path, { encoding: 'utf-8'});
+    const nquads = await rdfTransformString(data,path,'application/n-quads');
+
+    console.log(`# input nquads`);
+    console.log(nquads);
+
     const canonical = await canonize.canonize(nquads, {
         algorithm: 'RDFC-1.0',
         inputFormat: 'application/n-quads'
     });
 
+    console.log(`# output nquads`);
     console.log(canonical);
+
+    console.log(`# checksum`);
+    console.log(md5(canonical));
+}
+
+async function rdfTransformString(data, fileName, outType) {
+    const inStream = streamifyString(data);
+
+    // Guess the content-type from the path name
+    const quadStream = rdfParser.parse(inStream, { 
+        path:fileName ,
+    });
+
+    const outStream = rdfSerializer.serialize(quadStream, { contentType: outType });
+
+    return await stringifyStream(outStream);
 }
